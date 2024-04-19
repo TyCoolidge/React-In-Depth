27cbfe856bff5aac6bccd503265f48d0213c02e1
TyC <62319016+TyCoolidge@users.noreply.github.com>
2024-04-19 13:24:35 -0700

    ~class component lifecycle refresher

--- 14-class-components/src/App.js
+++ 14-class-components/src/App.js
@@ -1,11 +1,21 @@
-import Users from './components/Users';
+import UserFinder from "./components/UserFinder";
+import Users from "./components/Users";
+import UsersContext from "./store/users-context";
 
 function App() {
-  return (
-    <div>
-      <Users />
-    </div>
-  );
+	const DUMMY_USERS = [
+		{ id: "u1", name: "Max" },
+		{ id: "u2", name: "Manuel" },
+		{ id: "u3", name: "Julie" },
+	];
+	const userContext = {
+		users: DUMMY_USERS,
+	};
+	return (
+		<UsersContext.Provider value={userContext}>
+			<UserFinder />
+		</UsersContext.Provider>
+	);
 }
 
 export default App;
