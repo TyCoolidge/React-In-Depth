c0cebd559011cda155045a91627bf92959d2f7b3
TyC <62319016+TyCoolidge@users.noreply.github.com>
2024-05-07 14:04:58 -0700

    added useContext for nested components, lesson 271 - 9:50

--- 18-food-order-app/src/components/MealItem.jsx
+++ 18-food-order-app/src/components/MealItem.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { currencyFormatter } from "../utils/formatting";
+import Button from "./Button";
 
 function MealItem({ item }) {
 	return (
 		<li className="meal-item">
 			<article>
 				<img src={`http://localhost:3000/${item.image}`} alt={item.name} />
-				<h3>{item.name}</h3>
-				<p className="meal-item-description">{item.description}</p>
-				<p className="meal-item-price">${item.price}</p>
-				<button className="meal-item-actions">Add to Cart</button>
+				<div>
+					<h3>{item.name}</h3>
+					<p className="meal-item-price">
+						{currencyFormatter.format(item.price)}
+					</p>
+					<p className="meal-item-description">{item.description}</p>
+				</div>
+				<p className="meal-item-actions">
+					<Button>Add to Cart</Button>
+				</p>
 			</article>
 		</li>
 	);
---
+++ 18-food-order-app/src/store/CartContext.jsx
@@ -0,0 +1,87 @@
+import { createContext, useReducer } from "react";
+
+// don't need to set, just helps with planning and autocompletion
+const CartContext = createContext({
+	items: [],
+	addItem: (item) => {},
+	removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+	if (action.type === "ADD_ITEM") {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+
+		const updatedItems = [...state.items]; // need to make new copy to not effect passed in state; ex: if we have another conditional that runs after this
+		if (existingCartItemIndex > -1) {
+			const existingItem = state.items[existingCartItemIndex];
+			const updatedItem = {
+				...existingItem,
+				quantity: existingItem.quantity + 1,
+			};
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems.push({ ...action.item, quantity: 1 });
+		}
+		return { ...state, items: updatedItems };
+	}
+
+	if (action.type === "REMOVE_ITEM") {
+		//
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+
+		const existingItem = state.items[existingCartItemIndex];
+
+		const updatedItems = [...state.items];
+
+		if (existingItem.quantity === 1) {
+			updatedItems.splice(existingCartItemIndex, 1);
+		} else {
+			const updatedItem = {
+				...existingItem,
+				quantity: existingItem.quantity - 1,
+			};
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+		return { ...state, items: updatedItems };
+	}
+
+	return state;
+}
+
+export function CartContextProvider({ children }) {
+	const [cart, dispatchCartAction] = useReducer(cartReducer, {
+		items: [],
+	});
+
+	function addItem(item) {
+		dispatchCartAction({
+			type: "ADD_ITEM",
+			item,
+		});
+	}
+
+	function removeItem(id) {
+		dispatchCartAction({
+			type: "REMOVE_ITEM",
+			id,
+		});
+	}
+
+	const cartContextValue = {
+		items: cart.item,
+		addItem,
+		removeItem,
+	};
+
+	return (
+		<CartContext.Provider value={cartContextValue}>
+			{children}
+		</CartContext.Provider>
+	);
+}
+
+export default CartContext;
