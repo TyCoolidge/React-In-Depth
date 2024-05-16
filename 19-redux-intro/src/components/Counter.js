f9283c50d4fc9e62534ceed7ea472b6904934c76
TyC <62319016+TyCoolidge@users.noreply.github.com>
2024-05-16 15:18:07 -0700

    added redux-toolkit

--- 19-redux-intro/src/components/Counter.js
+++ 19-redux-intro/src/components/Counter.js
@@ -1,28 +1,39 @@
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
-console.log("🚀 ~ classes:", classes);
+import { counterActions } from "../store";
+console.log("🚀 ~ counterActions:", counterActions);
 
 const Counter = () => {
-	const counter = useSelector((state) => state.counter);
+	const { counter, showCounter } = useSelector((state) => state.counterReducer);
+
 	const dispatch = useDispatch();
 	console.log("🚀 ~ Counter ~ counter:", counter);
+	console.log("🚀 ~ Counter ~ showCounter:", showCounter);
 
 	const incrementHandler = () => {
-		dispatch({ type: "increment" });
+		dispatch(counterActions.increment());
+	};
+
+	const increaseHandler = () => {
+		dispatch(counterActions.increase(4)); //will pass value as the following object; {payload: 4}
 	};
 
 	const decrementHandler = () => {
-		dispatch({ type: "decrement" });
+		dispatch(counterActions.decrement());
 	};
 
-	const toggleCounterHandler = () => {};
+	const toggleCounterHandler = () => {
+		dispatch(counterActions.toggle());
+	};
 
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>-- {counter} --</div>
+			{showCounter && <div className={classes.value}>-- {counter} --</div>}
 			<div>
 				<button onClick={incrementHandler}>Increment</button>
+				<button onClick={increaseHandler}>Increment by 5</button>
+
 				<button onClick={decrementHandler}>Decrement</button>
 			</div>
 			<button onClick={toggleCounterHandler}>Toggle Counter</button>
--- 19-redux-intro/src/store/index.js
+++ 19-redux-intro/src/store/index.js
@@ -1,14 +1,55 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-	if (action.type === "increment") {
-		return { counter: state.counter + 1 };
-	}
-	if (action.type === "decrement") {
-		return { counter: state.counter - 1 };
-	}
-	return state;
-};
-const store = createStore(counterReducer);
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+	name: "counter",
+	initialState: initialState,
+	reducers: {
+		// even though we change state here, it is immutable under the hook in RTK
+		increment(state) {
+			state.counter++;
+		},
+		increase(state, action) {
+			state.counter = state.counter + action.payload;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		toggle(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
+
+// // NORMAL REDUX
+// const counterReducer = (state = initialState, action) => {
+// 	// never mutate state in normal redux or useState
+// 	if (action.type === "increment") {
+// 		return { ...state, counter: state.counter + 1 };
+// 	}
+
+// 	if (action.type === "increase") {
+// 		return { ...state, counter: state.counter + action.value };
+// 	}
+
+// 	if (action.type === "decrement") {
+// 		return { ...state, counter: state.counter - 1 };
+// 	}
+
+// 	if (action.type === "toggle") {
+// 		return { ...state, showCounter: !state.showCounter };
+// 	}
+// 	return state;
+// };
+
+export const counterActions = counterSlice.actions;
+
+const store = configureStore({
+	reducer: {
+		counterReducer: counterSlice.reducer,
+	},
+});
 
 export default store;
