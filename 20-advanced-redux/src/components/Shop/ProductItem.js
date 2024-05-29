c4db378eccadb2c982d32a2860656035a3b32a2b
TyC <62319016+TyCoolidge@users.noreply.github.com>
2024-05-29 13:25:14 -0700

    redux refresher

--- 20-advanced-redux/src/components/Shop/ProductItem.js
+++ 20-advanced-redux/src/components/Shop/ProductItem.js
@@ -1,23 +1,32 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+	const { title, price, description } = props;
+	const dispatch = useDispatch();
+	const { items } = useSelector((state) => state.cart);
+	console.log("🚀 ~ ProductItem ~ items:", items);
 
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+	const handleAddToCart = () => {
+		dispatch(cartActions.addToCart({ id: 1, price: 10 }));
+	};
+
+	return (
+		<li className={classes.item}>
+			<Card>
+				<header>
+					<h3>{title}</h3>
+					<div className={classes.price}>${price.toFixed(2)}</div>
+				</header>
+				<p>{description}</p>
+				<div className={classes.actions}>
+					<button onClick={handleAddToCart}>Add to Cart</button>
+				</div>
+			</Card>
+		</li>
+	);
 };
 
 export default ProductItem;
